d983344f8b098bc7590c3915b0b4728f434a94a3
knoorbakhsh <45353778+kimianoorbakhsh@users.noreply.github.com>
2021-10-03 18:46:56 +0330

    added reference

--- src/hf_utils.py
+++ src/hf_utils.py
@@ -22,6 +22,12 @@ def read_data_test(path):
         data = [xy for xy in data if len(xy) == 2]
     return data
 
+def read_data_test_new(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        lines = [line.rstrip() for line in f]
+        data = [xy.split('\t') for xy in lines]
+        data = [xy for xy in data if len(xy) == 2]
+    return data
 
 def postprocess_text(preds, labels):
     preds = [pred.strip() for pred in preds]
@@ -74,6 +80,24 @@ def create_dataset_test(path, language):
     dataset = Dataset.from_pandas(df)
     return dataset
 
+def create_dataset_test_new(path, language):
+    data = read_data_test_new(path)
+    text = []
+    label = []
+    for i in range(len(data)):
+        text.append(data[i][0])
+        label.append(data[i][1])
+    raw_datasets = [{'en': text[i], language: label[i]}
+                    for i in range(len(text))]
+
+    raw_datasets_t = {}
+    for i in range(len(raw_datasets)):
+        raw_datasets_t.setdefault('translation', []).append({'translation': raw_datasets[i]})
+
+    df = pd.DataFrame.from_dict(raw_datasets_t['translation'])
+    dataset = Dataset.from_pandas(df)
+    return dataset
+
 
 evaluationType = Enum('evaluationType', 'Training Validation Test')
 
@@ -86,23 +110,27 @@ def evaluation_function(totalNumberOfEvaluation, tokenized_datasets, evalType, t
         text = [tokenized_datasets['translation'][i]['en'] for i in range(
             j_batchIndex * batch_size, (j_batchIndex+1) * batch_size)]
         input_batch = tokenizer(text, return_tensors="pt", padding=True)
-        outputs = model.generate(**input_batch.to(device='cuda'), num_beams = num_beams)
-        decoded_batch = [tokenizer.decode(
-            t, skip_special_tokens=True) for t in outputs]
-        for k_indexInsideBatch in range(batch_size):
-            decoded = decoded_batch[k_indexInsideBatch]
-            ii_indexInWhole = j_batchIndex * batch_size + k_indexInsideBatch
-            actual = tokenized_datasets['translation'][ii_indexInWhole][language]
-            try:
-                actual_s = convert_to_sympy(actual, env)
-                decoded_s = convert_to_sympy(decoded, env)
-                res = True if simplify(
-                    decoded_s - actual_s, seconds=1) == 0 else False
-                if res == True:
-                    count_trueEstimation += 1
-            except:
-                count_nonMathExpressionEstimation += 1
-                continue
+        try:
+            outputs = model.generate(**input_batch.to(device='cuda'), num_beams = num_beams)
+            decoded_batch = [tokenizer.decode(
+                t, skip_special_tokens=True) for t in outputs]
+            for k_indexInsideBatch in range(batch_size):
+                decoded = decoded_batch[k_indexInsideBatch]
+                ii_indexInWhole = j_batchIndex * batch_size + k_indexInsideBatch
+                actual = tokenized_datasets['translation'][ii_indexInWhole][language]
+                try:
+                    actual_s = convert_to_sympy(actual, env)
+                    decoded_s = convert_to_sympy(decoded, env)
+                    res = True if simplify(
+                        decoded_s - actual_s, seconds=1) == 0 else False
+                    if res == True:
+                        count_trueEstimation += 1
+                except:
+                    count_nonMathExpressionEstimation += 1
+                    continue
+        except:
+            totalNumberOfEvaluation -= 1
+            continue
     print(evalType.name, "Accuracy:", 100 * count_trueEstimation/totalNumberOfEvaluation)
     print("NumberOfFalseEstimation", count_nonMathExpressionEstimation)
 
--- src/model/transformer.py
+++ src/model/transformer.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2020-present, Facebook, Inc.
 # All rights reserved.
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
+##############################################################################
+## Components from https://github.com/facebookresearch/SymbolicMathematics ##
+#############################################################################
 
 from logging import getLogger
 import math
