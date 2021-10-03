1c5e8eeae72cd8a09203bd1a6f705c3e18327823
knoorbakhsh <45353778+kimianoorbakhsh@users.noreply.github.com>
2021-10-03 18:32:21 +0330

    removed compute metrics

--- trainer.py
+++ trainer.py
@@ -30,29 +30,6 @@ def preprocess_function_new(examples):
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
-
-def compute_metrics(eval_preds):
-    preds, labels = eval_preds
-    if isinstance(preds, tuple):
-        preds = preds[0]
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-
-    # Replace -100 in the labels as we can't decode them.
-    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # Some simple post-processing
-    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
-
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-    result = {"bleu": result["score"]}
-
-    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
-    result["gen_len"] = np.mean(prediction_lens)
-    result = {k: round(v, 4) for k, v in result.items()}
-    return result
-
-
 if torch.cuda.is_available():
     device = 'cuda'
 else:
